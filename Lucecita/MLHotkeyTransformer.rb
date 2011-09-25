7602a8bf9d4150c8e6ca6af41a545019292b9e41
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2011-09-25 17:01:31 -0500

    Making the hotkey transformer to use lowercase letters to be used in menu item's keyEquivalent.

--- Lucecita/MLHotkeyTransformer.rb
+++ Lucecita/MLHotkeyTransformer.rb
@@ -6,29 +6,35 @@
 #  Copyright 2011 UNAM. All rights reserved.
 #
 
-class MLHotkeyTransformer
+class MLHotkeyTransformer < NSValueTransformer
   KEYCODES = {
   0x1D => "0", 0x12 => "1", 0x13 => "2", 0x14 => "3",
   0x15 => "4", 0x17 => "5", 0x16 => "6", 0x1A => "7",
-  0x1C => "8", 0x19 => "9", 0x00 => "A", 0x0B => "B",
-  0x08 => "C", 0x02 => "D", 0x0E => "E", 0x03 => "F",
-  0x05 => "G", 0x04 => "H", 0x22 => "I", 0x26 => "J",
-  0x28 => "K", 0x25 => "L", 0x2E => "M", 0x2D => "N",
-  0x1F => "O", 0x23 => "P", 0x0C => "Q", 0x0F => "R",
-  0x01 => "S", 0x11 => "T", 0x20 => "U", 0x09 => "V",
-  0x0D => "W", 0x07 => "X", 0x10 => "Y", 0x06 => "Z",
+  0x1C => "8", 0x19 => "9", 0x00 => "a", 0x0B => "b",
+  0x08 => "c", 0x02 => "d", 0x0E => "e", 0x03 => "f",
+  0x05 => "g", 0x04 => "h", 0x22 => "o", 0x26 => "j",
+  0x28 => "k", 0x25 => "l", 0x2E => "m", 0x2D => "n",
+  0x1F => "o", 0x23 => "p", 0x0C => "q", 0x0F => "r",
+  0x01 => "s", 0x11 => "t", 0x20 => "u", 0x09 => "v",
+  0x0D => "w", 0x07 => "x", 0x10 => "y", 0x06 => "z",
   0x2a => "\\"
   }
+  
+  KEYCHARS = KEYCODES.invert
   
   def self.transformedValueClass
     NSString
   end
 
   def self.allowsReverseTransformation
-    false
+    true
   end
 
   def transformedValue(value)
     KEYCODES[value]
+  end
+
+  def reverseTransformedValue(value)
+    KEYCHARS[value]
   end
 end
